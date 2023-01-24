450e8bb9b93fe5801ca8487e65cd19d6a569ec3d
Tarreazola19 <123480909+Tarreazola19@users.noreply.github.com>
2023-01-24 09:49:54 -0600

    Create hello.kt

---
+++ week01/hello.kt
@@ -0,0 +1,8 @@
+/**
+ * This is my first Kotling file/ Tlanextli Arreazola
+ */
+
+fun main() {
+    println("Hello, world!!!");
+    println("I am learning Kotlin,");
+    println("I love rainy days")
